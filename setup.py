be138d91b6adb2b550afc4457578d8188ac4e8b9
ianre657 <ianre657@gmail.com>
2017-09-25 20:21:20 +0800

    auto install pip

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
 # Setup tool for IanChen
 
+# Check python version
+def check_python_version():
+    import sys #temporarily import sys module
+    if sys.version_info.major < 3:
+        print("require Python 3 to run this code")
+        print("run \"python3 setup.py\"")
+        exit(1)
+check_python_version()
+
 import os, os.path
 import filecmp
-from pathlib import Path
 import shutil
-
+from pathlib import Path # python3 only 
 
 print("Initializing")
 
 sysname = os.uname().sysname.upper()
-
 HOMEDIR = Path.home()
 #HOMEDIR = Path.cwd().parent
 CURDIR = Path.cwd()
-print( "Home dir ={}".format(HOMEDIR))
 
 
 # bash file
@@ -21,13 +27,18 @@ if "Darwin".upper() in sysname:
     bash_file = ".bash_profile"
 elif "FreeBSD".upper() in sysname:
     bash_file = ".bash_profile"
+    pkg_manager = 'pacman'
 else:
     print("unknown system type")
     exit(1)
 
-def install_vim_plugin():
-    os.system('vim -E  -c PlugInstall -c PlugClean -c q -c q')
     
+
+def program_exists( program_name ):
+    if type( program_name ) is not str:
+        raise TypeError('arg:program_name need to be string type')
+    from shutil import which 
+    return which(program_name) is not None 
 
 
 if __name__ =="__main__":
@@ -39,7 +50,8 @@ if __name__ =="__main__":
         if (HOMEDIR/filename).exists() :
             if filecmp.cmp( CURDIR/filename , HOMEDIR/filename )==False:
                 # Files not the same, need to backup
-                print("Back up: ~/{} as: ~/{}".format(str(filename), str(filename)+'.old'))
+                print("Back up: ~/{} as: ~/{}"\
+                        .format(str(filename), str(filename)+'.old'))
                 (HOMEDIR/filename).rename( str(HOMEDIR/filename)+'.old')
 
                 print("Create: {}".format(filename))
@@ -52,12 +64,13 @@ if __name__ =="__main__":
     # Vim Plugin manager
         # Use vim-plug as defualt plugin manager 
     print("Install vim-plug,(Plugin manager for vim)")
-    os.system('curl -fLo {}/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'.format(str(HOMEDIR)))
+    os.system('curl -s -fLo {}/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'\
+    .format(str(HOMEDIR)))
 
     print("Install and upgrade all vim plugins")
     # Update all Plugins
-    install_vim_plugin()
+    os.system('vim -E -c PlugInstall -c PlugClean -c q -c q')
 
     print("Setup finished")
     print(r'''
@@ -68,3 +81,33 @@ if __name__ =="__main__":
     |___(____  /___|  /  \______  /___|  /\___  >___|  /
              \/     \/          \/     \/     \/     \/
     ''')
+
+    if not program_exists( 'pip3' ):
+
+        while True:
+            usr_ans = input( "Install pip? (yes/no) [no]:").upper()
+            if usr_ans == '': usr_ans = 'N'
+
+            if usr_ans not in ['YES','Y','NO','N']:
+                continue
+            else:
+                if usr_ans in ['YES', 'Y']:
+                    if not program_exists('curl'):
+                        raise Exception('Need to install curl')
+                    print("Downloading 'get-pip.py'...")
+                    os.system("curl -sfk 'https://bootstrap.pypa.io/get-pip.py'\
+                            --output {}/get-pip.py".format(CURDIR))
+
+                    print("ROOT password is required to installing pip")
+                    os.system("sudo -k python3 {}/get-pip.py".format(CURDIR))
+                    print("Installed pip successfully")
+                    print("Remove get-pip.py")
+                    os.system("sudo rm {}/get-pip.py".format(CURDIR))
+                    break
+
+                elif usr_ans in ['NO','N']:
+                    break
+
+
+
+
