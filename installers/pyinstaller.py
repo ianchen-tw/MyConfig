de85e6fc3088fe1c30eee418fc37a7d726ac1473
Ian Chen <ianre657@gmail.com>
2018-03-05 14:02:44 +0800

    修bug

--- installers/pyinstaller.py
+++ installers/pyinstaller.py
@@ -72,7 +72,8 @@ def ask():
     def ask_and_store(program):
         if user_confirm("Install {}? (yes/no) [yes]:".format(program), default_ans=True)is True:
             install_dict[program] = True
-    ask_and_store('pip3')
+    if not exists_program( 'pip3' ):
+        ask_and_store('pip3')
     ask_and_store('pipenv')
     ask_and_store('pyenv')
 
