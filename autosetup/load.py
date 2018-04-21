47a72d1d6d30ecb2d04b817360ca6049f363e42d
Ian Chen <ianre657@gmail.com>
2018-04-21 20:53:14 +0800

    detect non-exist config files

--- autosetup/load.py
+++ autosetup/load.py
@@ -1,5 +1,7 @@
+import os
+
 from .config import import_dir
-from .util import cp_with_backup
+from .util import cp_with_backup, user_confirm
 
 from pathlib import Path
 from .globalinfo import CURDIR
@@ -21,8 +23,22 @@ def main():
       cp_with_backup(src_file=home/file,des_folder=CURDIR/'dotfiles')
     except FileNotFoundError:
       print("ERROR : cannot find file {}".format(home/file))
+  load_fish_configs()
 
+
+def load_fish_configs():
   move_fish_cofig_file(fishdir=home/'.config/fish', destdir=CURDIR/'fish')
   
+  # delete file in package but not currently in user's configuration directories
+  dir_to_copy = ['functions','conf.d']
+  for directory in dir_to_copy:
+    for dest_file in os.listdir( f'{CURDIR}/fish/{directory}' ):
+      if not os.path.isfile(f'{home}/.config/fish/{directory}/{dest_file}'):
+        print(f"File:{CURDIR}/fish/{directory}/{dest_file}")
+        print(f"  exist in target dir but not in src dir:{home}/.config/fish/{directory}")
+        if user_confirm('>> delete this file? (yes/no) [no]:')is True:
+          os.remove(f'{CURDIR}/fish/{directory}/{dest_file}')
+
+
 if __name__ =='__main__':
   main()
